df131974b06169f15523ecf584b2ffc28136b113
changzhou <changzhou15@gmail.com>
2017-06-11 01:07:44 -0700

    client side implementation 1
    1. Created templates for each webpage(view).
    2. Created controllers for each view.
    3. Bound model to controllers and views.
    4. Configured routing.
    5. Implemented event handlers in controllers.
    TODO:
    1. Finish templates and controllers for widgets.
    2. Update href links in templates.

---
+++ public/assignment/views/widget/controllers/widget-new.controller.client.js
@@ -0,0 +1,40 @@
+(function () {
+    angular
+        .module('WebAppMaker')
+        .controller('widgetNewController', widgetNewController);
+
+    function widgetNewController($routeParams,
+                                 $sce,
+                                 widgetService) {
+        var model = this;
+
+        model.userId = $routeParams['userId'];
+        model.websiteId = $routeParams['websiteId'];
+        model.pageId = $routeParams['pageId'];
+        model.trust = trust;
+        model.getYouTubeEmbedUrl = getYouTubeEmbedUrl;
+        model.widgetUrl = widgetUrl;
+
+        function init() {
+            model.widgets = widgetService.findWidgetByPageId(model.pageId);
+        }
+        init();
+
+        function widgetUrl(widget) {
+            var url = 'views/widget/templates/widget-'+widget.widgetType.toLowerCase()+'.view.client.html';
+            return url;
+        }
+
+        function getYouTubeEmbedUrl(linkUrl) {
+            var embedUrl = "https://www.youtube.com/embed/";
+            var linkUrlParts = linkUrl.split('/');
+            embedUrl += linkUrlParts[linkUrlParts.length - 1];
+            return $sce.trustAsResourceUrl(embedUrl);
+        }
+        
+        function trust(html) {
+            // scrubbing the html
+            return $sce.trustAsHtml(html);
+        }
+    }
+})();
